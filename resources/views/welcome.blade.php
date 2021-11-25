3cf1308363b5efac4bb6fae200c632e4cb27f3d5
patriciax <yively.pa@gmail.com>
2021-11-25 15:16:03 -0400

    fix logo /imagem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,111 +1,113 @@
 @extends('layouts.login')
 
 @section('content')
-    <div class="login_admin " id="dev-login">
+<div class="login_admin " id="dev-login">
 
-        <div class="row">
-            <div class="login100-more mask col-md-6"
-                style="background-image: url('login-bg.jpg');">
-
-
-               <!---- <p>Bienvenido a Aidacaceres CMS</p>--->
-            </div>
-            <div class="login100-form validate-form col-md-6">
-
-                <p> Content Management System </p>
+    <div class="row">
+        <div class="login100-more mask col-md-6" style="background-image: url('login-bg.jpg');">
 
 
-                <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" v-model="email">
-                    <span class="focus-input100"></span>
-                    <span class="label-input100">Email</span>
-                </div>
+            <!---- <p>Bienvenido a Aidacaceres CMS</p>--->
+        </div>
+        <div class="login100-form validate-form col-md-6">
+
+            <img style="    width: 180px;
+    height: auto;
+    padding: 0 0 0.5rem;" src=" {{ url('logo_blue.svg') }}" alt="">
+            <p> Content Management System </p>
 
 
-                <div class="wrap-input100 validate-input">
-                    <input class="input100" type="password" v-model="password">
-                    <span class="focus-input100"></span>
-                    <span class="label-input100">Password</span>
-                </div>
-
-
-
-
-                <div class="container-login100-form-btn">
-                    <button class="login100-form-btn" @click="login()">
-                        Entrar
-                    </button>
-                </div>
-
+            <div class="wrap-input100 validate-input">
+                <input class="input100" type="text" v-model="email">
+                <span class="focus-input100"></span>
+                <span class="label-input100">Email</span>
             </div>
 
+
+            <div class="wrap-input100 validate-input">
+                <input class="input100" type="password" v-model="password">
+                <span class="focus-input100"></span>
+                <span class="label-input100">Password</span>
+            </div>
+
+
+
+
+            <div class="container-login100-form-btn">
+                <button class="login100-form-btn" @click="login()">
+                    Entrar
+                </button>
+            </div>
 
         </div>
 
+
     </div>
+
+</div>
 @endsection
 
 
 @push("scripts")
 
 <script type="text/javascript">
-const app = new Vue({
-    el: '#dev-login',
-    data() {
-        return {
-            email: "",
-            password: ""
-        }
-    },
-    methods: {
+    const app = new Vue({
+        el: '#dev-login',
+        data() {
+            return {
+                email: "",
+                password: ""
+            }
+        },
+        methods: {
 
-        login() {
+            login() {
 
-            axios.post("{{ url('/login') }}", {
-                    email: this.email,
-                    password: this.password
-                })
-                .then(res => {
+                axios.post("{{ url('/login') }}", {
+                        email: this.email,
+                        password: this.password
+                    })
+                    .then(res => {
 
-                    if (res.data.success == true) {
+                        if (res.data.success == true) {
 
-                        swal({
-                            title: "Excelente!",
-                            text: res.data.msg,
-                            icon: "success"
-                        });
-                        this.email = ""
-                        this.password = ""
+                            swal({
+                                title: "Excelente!",
+                                text: res.data.msg,
+                                icon: "success"
+                            });
+                            this.email = ""
+                            this.password = ""
 
-                        if (res.data.role_id == 2) {
+                            if (res.data.role_id == 2) {
 
-                            window.location.href = "{{ url('/') }}"
+                                window.location.href = "{{ url('/') }}"
 
 
-                        } else if (res.data.role_id == 1)
-                            window.location.href = res.data.url
+                            } else if (res.data.role_id == 1)
+                                window.location.href = res.data.url
 
-                    } else {
-                        alert(res.data.msg)
-                    }
+                        } else {
+                            alert(res.data.msg)
+                        }
 
-                })
-                .catch(err => {
+                    })
+                    .catch(err => {
 
-                    $.each(err.response.data.errors, function(key, value) {
-                        alert(value)
+                        $.each(err.response.data.errors, function(key, value) {
+                            alert(value)
+                        })
+
                     })
 
-                })
+            }
+
+        },
+        created() {
+
 
         }
-
-    },
-    created() {
-
-
-    }
-});
+    });
 </script>
 
 @endpush
